9375f45811cddda61bcb65e579e322db1a2363ad
鈴木悠太/Yuta Suzuki <104573801+xsuz@users.noreply.github.com>
2024-05-18 06:57:03 +0900

    回路図&PCBを追加

--- src/twelite/twelite-app.cpp
+++ src/twelite/twelite-app.cpp
@@ -3,15 +3,13 @@
 #include <NWK_SIMPLE>
 #include "SensorPacket.h"
 #include "client.h"
-#include <bno055/BNO055_support.h>
-#define P
+
 /*** Config part */
 // application ID
-const uint32_t APP_ID = 0x96fb64cd;
+const uint32_t APP_ID = 0x1234abcd;
 
 // channel
-const uint8_t CHANNEL = 7;
-bno055_t bno;
+const uint8_t CHANNEL = 13;
 
 /*** setup procedure (run once at cold boot) */
 void setup()
@@ -25,66 +23,91 @@ void setup()
 		<< TWENET::rx_when_idle();	// open receive circuit (if not set, it can't listen packts from others)
 
 	// Register Network
-	#ifdef P
 	auto &&nwksmpl = the_twelite.network.use<NWK_SIMPLE>();
 	nwksmpl << NWK_SIMPLE::logical_id(Client::Parent) // set Logical ID. (0x00 means a parent device)
 			<< NWK_SIMPLE::repeat_max(3);	// can repeat a packet up to three times. (being kind of a router)
-	#else
-	auto &&nwksmpl = the_twelite.network.use<NWK_SIMPLE>();
-	nwksmpl << NWK_SIMPLE::logical_id(Client::Addr::Children) // set Logical ID. (0x00 means a parent device)
-			<< NWK_SIMPLE::repeat_max(3);	// can repeat a packet up to three times. (being kind of a router)
-	#endif
 
 	the_twelite.begin(); // start twelite!
-	Analogue.setup(true);
-	Analogue.begin(pack_bits(PIN_ANALOGUE::A1), 50);
-	Serial.begin(115200);
-	BNO_Init(&bno);
-	delay(1000);
-#ifdef P
-	DeviceData::MainData data;
-	Client::transmit<DeviceData::MainData>(
-		DeviceData::DeviceID::MainBoard,
-		data,
-		millis(),
-		(float)analogRead_mv(PIN_ANALOGUE::A1)*2.0/1000.0,
-		Client::Addr::Children);
-	delay(200);
-	Serial << "sending..." << mwx::crlf;
-#endif
 }
 
 /*** loop procedure (called every event) */
 void loop()
 {
-#ifndef P
-	DeviceData::PitotData data;
-	data.velocity = 7.2;
-	Client::transmit<DeviceData::PitotData>(
-		DeviceData::DeviceID::Pitot,
-		data,
-		millis(),
-		(float)analogRead_mv(PIN_ANALOGUE::A1)*2.0/1000.0,
-		Client::Addr::Parent);
-	Serial << ".";
-	delay(200);
-#endif
 }
 
 void on_rx_packet(packet_rx &rx, bool_t &handled)
 {
-#ifdef P
-	// Client::on_rx_packet(rx);
-	// auto packet = rx.get_payload();
-	// if (packet[0] == DeviceData::DeviceID::Pitot)
-	// {
-	// 	DeviceData::SensorPacket<DeviceData::PitotData> spkt;
-	// 	expand_bytes(packet.begin(), packet.end(), spkt.bytes);
-	// 	Serial << format("id:%x timestamp:%d voltage:%f velocity%f",spkt.id,spkt.timestamp,spkt.voltage,spkt.data.velocity) << mwx::crlf;
-	// }
-#else
-	// Client::on_rx_packet(rx);
-#endif
+	auto pkt=rx.get_payload();
+	switch (pkt[0]&0xF0)
+	{
+	case DeviceData::DeviceID::Altitude:
+	{
+		union{
+			DeviceData::AltData data[LEN_ALT];
+			uint8_t bytes[sizeof(DeviceData::AltData[LEN_ALT])];
+		} spkt;
+		expand_bytes(pkt.begin(),pkt.end(),spkt.bytes);
+		for(int i=0;i<LEN_ALT;i++){
+			Serial << format("{'id':%x,'t':%d",spkt.data[i].id,spkt.data[i].timestamp)
+					<< format(",payload:{'alt':%f",spkt.data[i].alt)
+					<< format("}}") << mwx::crlf;
+		}
+	}break;
+	case DeviceData::DeviceID::IMU:
+	{
+		union{
+			DeviceData::IMUData data[LEN_IMU];
+			uint8_t bytes[sizeof(DeviceData::IMUData[LEN_IMU])];
+		} spkt;
+		expand_bytes(pkt.begin(),pkt.end(),spkt.bytes);
+		for(int i=0;i<LEN_IMU;i++){
+			Serial << format("{'id':%x,'t':%d",spkt.data[i].id,spkt.data[i].timestamp)
+					<< format(",payload:{'w':%d,'x':%d,'y':%d,'z':%d",spkt.data[i].w,spkt.data[i].x,spkt.data[i].y,spkt.data[i].z)
+					<< format("}}") << mwx::crlf;
+		}
+	}break;
+	case DeviceData::DeviceID::Pitot:
+	{
+		union{
+			DeviceData::PitotData data[LEN_PITOT];
+			uint8_t bytes[sizeof(DeviceData::PitotData[LEN_PITOT])];
+		} spkt;
+		expand_bytes(pkt.begin(),pkt.end(),spkt.bytes);
+		for(int i=0;i<LEN_PITOT;i++){
+			Serial << format("{'id':%x,'t':%d",spkt.data[i].id,spkt.data[i].timestamp)
+					<< format(",payload:{'v':%f",spkt.data[i].velocity)
+					<< format("}}") << mwx::crlf;
+		}
+	}break;
+	case DeviceData::DeviceID::ServoController:{
+		union{
+			DeviceData::ServoData data[LEN_SERVOCONTROLLER];
+			uint8_t bytes[sizeof(DeviceData::ServoData[LEN_SERVOCONTROLLER])];
+		} spkt;
+		expand_bytes(pkt.begin(),pkt.end(),spkt.bytes);
+		for(int i=0;i<LEN_SERVOCONTROLLER;i++){
+			Serial << format("{'id':%x,'t':%d",spkt.data[i].id,spkt.data[i].timestamp)
+					<< format(",payload:{'rudder':%f,'elevator':%f,'trim':%f,'status':%x",spkt.data[i].rudder,spkt.data[i].elevator,spkt.data[i].trim,spkt.data[i].status)
+					<< format("}}") << mwx::crlf;
+		}
+		}break;
+	case DeviceData::DeviceID::Tachometer:
+	{
+		union{
+			DeviceData::TachometerData data[LEN_TACHOMETER];
+			uint8_t bytes[sizeof(DeviceData::TachometerData[LEN_TACHOMETER])];
+		} spkt;
+		expand_bytes(pkt.begin(),pkt.end(),spkt.bytes);
+		for(int i=0;i<LEN_TACHOMETER;i++){
+			Serial << format("{'id':%x,'t':%d",spkt.data[i].id,spkt.data[i].timestamp)
+					<< format(",payload:{'pwr':%d,'rpm':%d",spkt.data[i].power,spkt.data[i].rpm)
+					<< format("}}") << mwx::crlf;
+		}
+	}break;
+	default:{
+		Serial << format("Unknown ID : %x",pkt[0]) << mwx::crlf;
+	}break;
+	}
 }
 
 /* Copyright (C) 2019-2021 Mono Wireless Inc. All Rights Reserved. *
